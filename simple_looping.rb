1416518c5ab07a3d9d16733f425c316a539d4721
Sally Crawford <sally.crawford@gmail.com>
2019-04-10 11:46:36 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -44,6 +44,6 @@ end
 def for_iterator(number_of_times)
   counter = 1..7
   for count in counter do
-    puts "Welcome to Flatiron School's Web Development Course!
+    puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
